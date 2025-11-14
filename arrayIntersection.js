5c00990fba44bf93ffa3d391a8226ae006293073
Hariprasad MN <hariprasadmn4274@gmail.com>
2025-11-14 18:34:13 +0530

    Clarify set1 creation in arrayIntersection function
    Added comment to clarify the purpose of set1.

--- arrayIntersection.js
+++ arrayIntersection.js
@@ -1,5 +1,5 @@
 function arrayIntersection(arr1,arr2){
-    let set1=new Set(arr1)
+    let set1=new Set(arr1) //creating new object from arr1 , so that object will remove all duplicates and store only uniqe numbers
     let set2=new Set(arr2)
 
     return [...set1].filter(items => set2.has(items))
