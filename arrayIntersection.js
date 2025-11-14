c1214d0f462152aebd16d32fd16226d064e071de
Hariprasad MN <hariprasadmn4274@gmail.com>
2025-11-14 18:51:30 +0530

    Improve comments in arrayIntersection function
    Clarified comment on the return statement for better understanding.

--- arrayIntersection.js
+++ arrayIntersection.js
@@ -2,7 +2,7 @@ function arrayIntersection(arr1,arr2){
     let set1=new Set(arr1) //creating new object from arr1 , so that object will remove all duplicates and store only uniqe numbers
     let set2=new Set(arr2)
 
-    return [...set1].filter(items => set2.has(items))
+    return [...set1].filter(items => set2.has(items))//[...set1] is JavaScript syntax that uses the spread operator to convert a Set object (set1) into an array.
 
 }
 console.log(arrayIntersection([1,2,3,4,5],[3,4,5,6,7,8]))
