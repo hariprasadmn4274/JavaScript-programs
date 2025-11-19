fa554c01489b23f3613acf1d92acadd34648bf78
Hariprasad MN <hariprasadmn4274@gmail.com>
2025-11-19 16:30:36 +0530

    Enhance comments in areAnagrams function
    Added comments to clarify the cleaning process of strings for anagram checking.

--- anagrams.js
+++ anagrams.js
@@ -1,5 +1,11 @@
 function areAnagrams(str1,str2){
-    let cleanStr1=str1.replace(/[^a-zA-Z]/g, '').toLowerCase().split('').sort().join('')     // split('') will covert string to arry, sort() for arry will sort, join('') arry will convert into string
+    let cleanStr1=str1.replace(/[^a-zA-Z]/g, '').toLowerCase().split('').sort().join('') 
+    
+    // split('') will covert string to arry, sort() for arry will sort, join('') arry will convert into string
+    //    /[^a-zA-Z]/g is a regular expression that matches any character that is not a letter (a-z or A-Z).,
+    //Without g: Only the first match is replaced.
+    //With g: Every match in the string is replaced.
+    
     let cleanStr2=str2.replace(/[^a-zA-Z]/g, '').toLowerCase().split('').sort().join('')
     if(cleanStr1===cleanStr2){
         console.log(`YES both ${str1} and ${str2} are Anagrames like ${cleanStr1} and ${cleanStr2}`)
