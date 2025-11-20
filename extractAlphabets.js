4c655402511bede92a0eeb5edd075cadc72a1d57
Hariprasad MN <hariprasadmn4274@gmail.com>
2025-11-20 14:46:28 +0530

    Update extractAlphabets.js

--- extractAlphabets.js
+++ extractAlphabets.js
@@ -3,7 +3,8 @@ function extractNumbers(str){
     let num=[]
     for(let char of str){
         if(/[a-zA-Z]/.test(char)){// if(/[^a-zA-Z]/.test(char)){ you can use this one also
-
+                                //    /[a-zA-Z]/ is a regular expression that matches any single letter from a to z (lowercase) or A to Z (uppercase).
+                                //.test(char) returns true if char matches the pattern (i.e., is a letter), otherwise false.
             num.push(char)
         }
     }
