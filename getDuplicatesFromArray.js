8aa6f694434253de04123c5a1df0bfc1498f6b76
Hariprasad MN <hariprasadmn4274@gmail.com>
2025-11-20 16:24:28 +0530

    Update getDuplicatesFromArray.js

--- getDuplicatesFromArray.js
+++ getDuplicatesFromArray.js
@@ -1,3 +1,5 @@
+//1.)getDuplicateNumbersFromArray
+
 function getDuplicatesFromArray(arr){
     let seen=new Set()
     let duplicates=[]
@@ -11,3 +13,24 @@ function getDuplicatesFromArray(arr){
     return duplicates
 }
 console.log(getDuplicatesFromArray([1,2,2,3,4,4,5,6]))
+
+
+
+//2.) getDuplicateLettersFromString
+
+function getDuplicateLettersFromString(str){
+
+    let set=new Set();
+    let duplicates=[];
+    for(letter of str){
+        if(set.has(letter)){
+           duplicates.push(letter);  
+        }
+        else{
+        set.add(letter);
+    }
+    }
+    return duplicates;
+
+}
+console.log(getDuplicateLettersFromString('programming'));
