2705f5a106194d9f569a6e566b208b7879387a01
Hariprasad MN <hariprasadmn4274@gmail.com>
2025-11-21 21:48:15 +0530

    Update reverseStringWithoutReverseMethod.js

--- reverseStringWithoutReverseMethod.js
+++ reverseStringWithoutReverseMethod.js
@@ -1,8 +1,8 @@
 function reverseStringwithoutReverseMethod(str){
     let cleanStr=str.replace(/[^a-zA-Z]/g, '').split('');
-        let reverseString='';
+        let reverseString=[];
     for(let i=cleanStr.length-1;i>=0;i--){// .length is array function gives number of elements it has. ex: hari has 4 elements
-        reverseString+=cleanStr[i]
+        reverseString+=cleanStr[i] //Using += turns it into a string
     }
     return reverseString;
 }
