d48a77f67c258be72c3396430a9f067ab66f8aa1
Hariprasad MN <hariprasadmn4274@gmail.com>
2025-11-23 16:35:05 +0530

    Implement second largest number finder function
    Add function to find the second largest number in an array.

--- secoundLorgestInArray.js
+++ secoundLorgestInArray.js
@@ -1,3 +1,24 @@
+function secondlorgestNumbArr(arr){
+    let lorgest=1;
+    let secLorgest=0
+    // console.log(arr)
+    
+    for(let char of arr){
+        
+        if(char > lorgest){
+            // console.log(char)
+            secLorgest=lorgest
+            lorgest=char;
+            
+        }
+    }
+    console.log(secLorgest)
+    return lorgest
+}
+console.log(secondlorgestNumbArr([1,2,2,3,3,5]))
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 function secoundLorgest(arr){
     if(arr.length<2){
         throw new console.Error('give valid array');
