79b10683d463b27332eb0028a3c7498e4e51af78
Hariprasad MN <hariprasadmn4274@gmail.com>
2026-05-14 15:36:28 +0530

    Implement validation for curly bracket sequences
    Added a function to validate curly bracket sequences.

--- Check if a Bracket Sequence is Properly Closed.js.js
+++ Check if a Bracket Sequence is Properly Closed.js.js
@@ -16,3 +16,24 @@ function isValidBracketSequence(str) {
 }
 
 console.log(isValidBracketSequence("(())()")); // true
+
+
+or
+
+function isValidBracketSequence(str){
+let count=0
+    for(let br of str){
+        if(br ==='{'){
+            count++;
+        }
+        else if(br=='}'){
+            if(count==0){
+                return false;
+            }else{
+                count--;
+            }
+        }
+    }
+    return count==0;
+}
+console.log(isValidBracketSequence("{}")); // true
