dafbebf6b27952f24b3da1719a04cdbcf2dc1b02
Hariprasad MN <hariprasadmn4274@gmail.com>
2026-05-15 09:37:32 +0530

    Update fibonacciSeries.js

--- fibonacciSeries.js
+++ fibonacciSeries.js
@@ -17,3 +17,23 @@ function fibonacciSeries(n){
         
 }
 console.log(fibonacciSeries(10))
+
+
+or
+
+function fabinoseriesofNum(num){
+   if(num<=0){
+       return null;
+   } 
+   if(num==1){
+       return 0;
+   }
+
+   let series=[0,1];
+   
+   for(let i=2;i<num;i++){
+       series.push(series[i-1]+series[i-2]);
+   }
+    return series;
+}
+console.log(fabinoseriesofNum(2))
